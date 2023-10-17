6046dfb1f5827557c78fde59862f056eb681e4d6
LinconCS <linconsantos@gmail.com>
2023-10-17 00:54:31 -0300

    node-package-json

--- exercicios/par-ou-impar.js
+++ exercicios/par-ou-impar.js
@@ -1,12 +1,25 @@
 // Função para gerar número aleatório entre min e max (incluindo ambos)
-function getRndInteger(min, max) {
+  function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
   
   // Obtém a escolha do jogador via argumentos
   const [, , jogadorEscolha, jogadorNumero] = process.argv;
+
+  // Verificar se o jogador fez uma escolha válida
+  if (jogadorEscolha !== "par" && jogadorEscolha !== "impar") {
+  console.log("Por favor, escolha 'par' ou 'impar'.");
+  process.exit(1);
+  }
   
   const jogadorNumeroInt = parseInt(jogadorNumero);
+
+  // Não permitir números menores que zero e nem deixar número vazio
+  if (isNaN(jogadorNumeroInt) || jogadorNumeroInt < 0) {
+    console.log("Por favor, escolha um número positivo válido.");
+    process.exit(1);
+  }
+
   const jogadorPar = jogadorEscolha === "par";
   
   // O computador escolhe o oposto da escolha do jogador
