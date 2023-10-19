fb2b096c56faed9a828f53babaa9c94a40d1f8d6
LinconCS <linconsantos@gmail.com>
2023-10-19 02:45:36 -0300

    alterações apis e express

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,96 @@
-import { users, products, createUser, getAllUsers, createProduct, getAllProducts, searchProductsByName } from './database';
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import { 
+  users, 
+  products, 
+  createUser, 
+  getAllUsers, 
+  createProduct, 
+  getAllProducts, 
+  searchProductsByName 
+} from './database';
 
-// console.log('Hello, TypeScript!');
+const app = express();
 
-// console.log(users);
-// console.log(products);
+app.use(express.json());
+app.use(cors());
 
-// console.table(users);
-// console.table(products);
+// Endpoints dos usuários
+app.post('/users', (req: Request, res: Response) => {
+  const { id, name, email, password } = req.body;
+  const result = createUser(id, name, email, password);
+  res.send(result);
+});
 
-// Exemplo de uso do createUser
-const createUserResult = createUser("u004", "Astrodev", "astrodev@email.com", "astrodev33");
-console.log(createUserResult);
+app.get('/users', (req: Request, res: Response) => {
+  const allUsers = getAllUsers();
+  res.json(allUsers);
+});
 
-// Exemplo de uso do getAllUsers
-const allUsers = getAllUsers();
-console.log('Todos os usuários:', allUsers);
+// Endpoints dos produtos
+app.post('/products', (req: Request, res: Response) => {
+  const { id, name, price, description, imageUrl } = req.body;
+  const result = createProduct(id, name, price, description, imageUrl);
+  res.send(result);
+});
 
-// Exemplo de uso do createProduct
-const createProductResult = createProduct("prod004", "SSD 2TB", 600, "Acelere seu sistema para poder desfrutar dos seus jogos com mais velocidade.", "https://picsum.photos/seed/Monitor/400");
-console.log(createProductResult);
+app.get('/products', (req: Request, res: Response) => {
+  const allProducts = getAllProducts();
+  res.json(allProducts);
+});
 
-// Exemplo de uso do getAllProducts
-const allProducts = getAllProducts();
-console.log('Todos os produtos:', allProducts);
+app.get('/products/search', (req: Request, res: Response) => {
+  const name = req.query.name as string;
+  const searchResults = searchProductsByName(name);
+  res.json(searchResults);
+});
 
-// Exemplo de uso do searchProductsByName
-const searchResults = searchProductsByName('gamer');
-console.log('Resultados da busca:', searchResults);
+// Endpoint de teste
+app.get('/ping', (req: Request, res: Response) => {
+  res.send('Pong!');
+});
+
+const PORT = process.env.PORT || 3003;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+// Get All Users
+app.get('/users', (req: Request, res: Response) => {
+  const allUsers = getAllUsers();
+  res.status(200).json(allUsers);
+});
+
+// Get All Products
+app.get('/products', (req: Request, res: Response) => {
+  const allProducts = getAllProducts();
+  res.status(200).json(allProducts);
+});
+
+// Refatorar o GET /products
+app.get('/product', (req: Request, res: Response) => {
+  const name = req.query.name as string;
+
+  if (name) {
+    const searchResults = searchProductsByName(name);
+    res.status(200).json(searchResults);
+  } else {
+    const allProducts = getAllProducts();
+    res.status(200).json(allProducts);
+  }
+});
+
+// Create User
+app.post('/users', (req: Request, res: Response) => {
+  const { id, name, email, password } = req.body;
+  const result = createUser(id, name, email, password);
+  res.status(201).send("Cadastro realizado com sucesso");
+});
+
+// Create Product
+app.post('/products', (req: Request, res: Response) => {
+  const { id, name, price, description, imageUrl } = req.body;
+  const result = createProduct(id, name, price, description, imageUrl);
+  res.status(201).send("Produto cadastrado com sucesso");
+});
